fa1de872267b459e80cc8e90a89a5b8b1693c453
John Bohlmann <johnmbohlmann@gmail.com>
2022-08-04 22:02:16 -0700

    build

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,3 +1,4 @@
 import { ExpressAppConfig } from "./middleware/express.app.config";
 import { Oas3AppOptions } from "./middleware/oas3.options";
 export declare function expressAppConfig(definitionPath: string, appOptions: Oas3AppOptions): ExpressAppConfig;
+export declare function test(): void;
--- dist/index.js
+++ dist/index.js
@@ -1,9 +1,13 @@
 'use strict';
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.expressAppConfig = void 0;
+exports.test = exports.expressAppConfig = void 0;
 const express_app_config_1 = require("./middleware/express.app.config");
 function expressAppConfig(definitionPath, appOptions) {
     return new express_app_config_1.ExpressAppConfig(definitionPath, appOptions);
 }
 exports.expressAppConfig = expressAppConfig;
+function test() {
+    console.log('test');
+}
+exports.test = test;
 //# sourceMappingURL=index.js.map
